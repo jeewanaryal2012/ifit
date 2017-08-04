815d9072597ec8cd6283d05cd46a4bcb89221eef
jeewanaryal <jeewanaryal@gmail.com>
2017-08-03 23:17:55 -0500

    iFit commit

---
+++ ifit/js/main.js
@@ -0,0 +1,12 @@
+
+/*
+This is a Design/Development test required from iFit company. This is a property of Jeewan Aryal.
+You are not allowed to use it in any places without my permission.
+Ask me at jeewanaryal@gmail.com before using any of the contents from here.
+Thanks.
+*/
+$(document).ready(function() {
+  $(".fa-search").on("click", function() {
+    $(".input-search").toggleClass("show-search-input"); //css("display", "inline-block");
+  });
+});
